4ad38c24cdbe879206cdfa527a09551f462689de
ranniellbanzonn <ranniellbanzon@gmail.com>
2025-04-18 13:58:45 +0800

    updated the copy

--- project-01-copy/app/page.tsx
+++ project-01-copy/app/page.tsx
@@ -1,29 +1,37 @@
 export default function Home() {
   return (
     <>
-      <main className="bg-black min-h-screen flex py-20 flex-col justify-center items-center">
-        <div className="bg-[#161616] text-white w-[26rem] rounded-xl py-10 text-center  items-center flex flex-col  px-10">
+      <main className="flex text-white justify-center  bg-black min-h-screen py-20  px-10">
+        <div className="flex flex-col justify-center items-center text-center w-[26rem] rounded py-10 bg-[#161616]">
           <img
             src="./avatar-jessica.jpeg"
-            className="w-[6rem] h-[6rem] rounded-full mb-6"
+            className="w-[6rem] rounded-full mb-6"
             alt=""
           />
           <h1 className="text-2xl font-bold">Jessica Randall</h1>
-          <p className="text-amber-500 text-xs font-bold mb-6">
+          <p className="text-xs text-amber-500 font-bold mb-6">
             London, United Kingdom
           </p>
           <p className="text-gray-400 mb-6">
             "Front-end Developer and avid reader."
           </p>
 
-          <div className="w-full flex flex-col gap-4">
-            <div className="bg-[#333333] rounded font-bold py-3">GitHub</div>
-            <div className="bg-[#333333] rounded font-bold py-3">
-              Frontend Mentor
+          <div className="w-full px-10 flex flex-col gap-4 font-bold ">
+            <div className="bg-[#333333] py-4 rounded">
+              <p>GitHub</p>
             </div>
-            <div className="bg-[#333333] rounded font-bold py-3">LinkedIn</div>
-            <div className="bg-[#333333] rounded font-bold py-3">Twitter</div>
-            <div className="bg-[#333333] rounded font-bold py-3">Instagram</div>
+            <div className="bg-[#333333] py-4 rounded">
+              <p>Frontend Mentor</p>
+            </div>
+            <div className="bg-[#333333] py-4 rounded">
+              <p>LinkedIn</p>
+            </div>
+            <div className="bg-[#333333] py-4 rounded">
+              <p>Twitter</p>
+            </div>
+            <div className="bg-[#333333] py-4 rounded">
+              <p>Instagram</p>
+            </div>
           </div>
         </div>
       </main>
