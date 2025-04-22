468c67d7b91b063d70905cf5205327f1c65fda78
ranniellbanzonn <ranniellbanzon@gmail.com>
2025-04-22 21:20:39 +0800

    added project section

--- project-01-copy/app/page.tsx
+++ project-01-copy/app/page.tsx
@@ -19,16 +19,12 @@ export default function Home() {
           </div>
         </div>
       </nav>
-      <main className="bg-black min-h-screen text-white py-20 flex justify-center text-center items-center">
-        <div className="flex justify-center flex-col bg-[#161616] w-[26rem] px-10 py-10 rounded-xl items-center">
-          <img
-            src="./avatar-jessica.jpeg"
-            className="w-[6rem] rounded-full mb-6"
-            alt=""
-          />
-          <h1 className="font-bold text-2xl">Jessica Randall</h1>
+      <main className="bg-black min-h-screen text-white py-20 flex px-10  flex-col justify-center text-center items-center">
+        <div className="flex justify-center flex-col bg-[#161616] w-[26rem] px-10 py-10 rounded-xl items-center mb-20">
+          <img src="./pfp.jpg" className="w-[6rem] rounded-full mb-6" alt="" />
+          <h1 className="font-bold text-2xl">Ranniell Banzon</h1>
           <p className="text-xs mb-6 font-bold text-amber-500">
-            London, United Kingdom
+            Valenzuela, Philippines
           </p>
           <p className="text-gray-400 mb-6">
             "Front-end Developer and avid reader."
@@ -61,7 +57,67 @@ export default function Home() {
             </a>
           </div>
         </div>
+        <h1 className="text-6xl text-amber-500 font-bold mb-10">My Projects</h1>
+        <div className="flex w-full gap-4 flex-wrap py-20">
+          <div className="flex-1 bg-[#333333]  flex justify-between items-center h-50 rounded-xl ">
+            <div className="flex text-left  flex-col gap-10 px-10">
+              <h1 className=" text-6xl">01</h1>
+              <p>Lorem ipsum, dolor sit amet consectetur</p>
+            </div>
+            <div className="px-10">LOGO HERE</div>
+          </div>
+          <div className="flex-1 bg-[#333333] flex justify-between items-center h-50 rounded-xl ">
+            <div className="flex text-left  flex-col gap-10 px-10">
+              <h1 className="text-6xl">02</h1>
+              <p>Lorem ipsum, dolor sit amet consectetur</p>
+            </div>
+            <div className="px-10">LOGO HERE</div>
+          </div>
+          <div className="flex-1  bg-[#333333] flex justify-between items-center h-50 rounded-xl ">
+            <div className="flex text-left  flex-col gap-10 px-10">
+              <h1 className="text-6xl">03</h1>
+              <p>Lorem ipsum, dolor sit amet consectetur</p>
+            </div>
+            <div className="px-10">LOGO HERE</div>
+          </div>
+          <div className="flex-1  bg-[#333333] flex justify-between items-center h-50 rounded-xl ">
+            <div className="flex text-left  flex-col gap-10 px-10">
+              <h1 className="text-6xl">04</h1>
+              <p>Lorem ipsum, dolor sit amet consectetur</p>
+            </div>
+            <div className="px-10">LOGO HERE</div>
+          </div>
+          <div className="flex-1  bg-[#333333] flex justify-between items-center h-50 rounded-xl ">
+            <div className="flex text-left  flex-col gap-10 px-10">
+              <h1 className="text-6xl">05</h1>
+              <p>Lorem ipsum, dolor sit amet consectetur</p>
+            </div>
+            <div className="px-10">LOGO HERE</div>
+          </div>
+          <div className="flex-1  bg-[#333333] flex justify-between items-center h-50 rounded-xl ">
+            <div className="flex text-left  flex-col gap-10 px-10">
+              <h1 className="text-6xl">06</h1>
+              <p>Lorem ipsum, dolor sit amet consectetur</p>
+            </div>
+            <div className="px-10">LOGO HERE</div>
+          </div>
+          <div className="flex-1  bg-[#333333] flex justify-between items-center h-50 rounded-xl ">
+            <div className="flex text-left  flex-col gap-10 px-10">
+              <h1 className="text-6xl">07</h1>
+              <p>Lorem ipsum, dolor sit amet consectetur</p>
+            </div>
+            <div className="px-10">LOGO HERE</div>
+          </div>
+          <div className="flex-1  bg-[#333333] flex justify-between items-center h-50 rounded-xl ">
+            <div className="flex text-left  flex-col gap-10 px-10">
+              <h1 className="text-6xl">08</h1>
+              <p>Lorem ipsum, dolor sit amet consectetur</p>
+            </div>
+            <div className="px-10">LOGO HERE</div>
+          </div>
+        </div>
       </main>
+
       <footer className="bg-[#161616] py-20 text-white px-10">
         <div className="flex justify-between items-center">
           <div className="flex gap-10">
